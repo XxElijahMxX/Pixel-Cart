79687ad757bc582ded151b5b8db00fe5d90f13ae
unknown <mwright7272@gmail.com>
2022-08-02 20:37:08 -0500

    finished payment page

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
               </div>
             }
           />
+
           <Route path="/login" element={<Login />} />
 
           <Route
--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Payment.css";
 import { useStateValue } from "../StateProvider/StateProvider";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -8,6 +10,9 @@ function Payment() {
   return (
     <div className="payment">
       <div className="paymentContainer">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
         <div className="paymentSection">
           <div className="paymentTitle">
             <h3>Delivery Address</h3>
@@ -15,11 +20,31 @@ function Payment() {
           <div className="paymentAddress">
             <p>{user?.email}</p>
             <p>123 Test Lane</p>
-            <p>Indianpolis, IN</p>
+            <p>Indianapolis, IN</p>
           </div>
         </div>
-        <div className="paymentSection"></div>
-        <div className="paymentSection"></div>
+        <div className="paymentSection">
+          <div className="paymentTitle">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="paymentItems">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                review={item.review}
+                price={item.price}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="paymentSection">
+          <div className="paymentTitle">
+            <h3>Payment Method</h3>
+          </div>
+          {/* Stripe Stuff */}
+        </div>
       </div>
     </div>
   );
