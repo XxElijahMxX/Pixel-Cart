9936f91bb15645aef84dc9600bcc0c1ddd50408f
unknown <mwright7272@gmail.com>
2022-08-03 23:28:11 -0500

    fix path to index file in server.js

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"), function(err) {
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"), function(err) {
     if (err) {
       res.status(500).send(err)
     }
