3e5117546a4dccf354e5cae04daf0bbd043f9b86
unknown <mwright7272@gmail.com>
2022-08-03 23:18:17 -0500

    add path to index.html to server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"), function(err) {
     if (err) {
       res.status(500).send(err)
     }
